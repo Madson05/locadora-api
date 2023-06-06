49bd8321921ceedd372ae49f4cee80a4c4e6b283
madson <madsonsilva2005@gmail.com>
2023-06-06 18:26:09 -0300

    criada a camada de service e começando a usar o conceito de responsal=bilidade unica do solid criada um classe service somente para criação

--- src/controllers/categories.controller.ts
+++ src/controllers/categories.controller.ts
@@ -1,19 +1,17 @@
 import { Request, Response } from "express";
 
 
-import createCategoryRepository from "../repositories/categories.repository";
+import categoriesService from "../repositories/categories.repository";
+import CreateCategoryService from "../services/CreateCategory.service";
 
 
 class categoriesController {
 
   async create(req: Request, res: Response) {
     const { name, description } = req.body;
-    const categoryAlreadyExists = await createCategoryRepository.findByName(name);
-    if(categoryAlreadyExists){
-      return res.status(400).json({error: "Category already exists"});
-    }
     
-    res.send(await createCategoryRepository.create(name, description))
+    
+    res.send(await new CreateCategoryService().execute(name, description));
 
   }
 
---
+++ src/services/CreateCategory.service.ts
@@ -0,0 +1,15 @@
+import categoriesRepository from "../repositories/categories.repository";
+
+class CreateCategoryService{
+
+  public async execute(name: string, description: string){
+    const categoryAlreadyExists = await categoriesRepository.findByName(name);
+    if(categoryAlreadyExists){
+      return {error: "Category already exists"};
+    }
+    await categoriesRepository.create(name, description);
+  }
+
+}
+
+export default  CreateCategoryService 
